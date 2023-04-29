cc5a60454fce689d4bd4232a7aeaf9b17e175a47
glennuke1 <132171380+glennuke1@users.noreply.github.com>
2023-04-30 02:50:55 +0300

    Add files via upload

---
+++ AttachablePartApi/AttachablePartApi/AttachComp.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using HutongGames.PlayMaker;
+using MSCLoader;
+
+namespace AttachablePartApi
+{
+    public class AttachComp : MonoBehaviour
+    {
+        public Transform pivot;
+        public bool on;
+        public bool screwable;
+
+        GameObject raycast_parent;
+        FsmGameObject raycast_object;
+
+        void Start()
+        {
+            raycast_parent = GameObject.Find("PLAYER").transform.FindChild("Pivot/AnimPivot/Camera/FPSCamera/1Hand_Assemble/Hand").gameObject;
+            raycast_object = PlayMakerFSM.FindFsmOnGameObject(raycast_parent, "PickUp").FsmVariables.FindFsmGameObject("RaycastHitObject");
+
+            if (SaveLoad.ReadValue<bool>(ModLoader.GetMod("AttachablePartApi"), "attached" + gameObject.name))
+            {
+                Attach();
+            }
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject == pivot.gameObject && gameObject.layer == LayerMask.NameToLayer("Wheel") && !on)
+            {
+                PlayMakerGlobals.Instance.Variables.GetFsmBool("GUIassemble").Value = true;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Attach();
+                    PlayMakerGlobals.Instance.Variables.GetFsmBool("GUIassemble").Value = false;
+                }
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject == pivot.gameObject)
+            {
+                PlayMakerGlobals.Instance.Variables.GetFsmBool("GUIassemble").Value = false;
+            }
+        }
+
+        void Update()
+        {
+            if (raycast_object.Value == gameObject && on)
+            {
+                PlayMakerGlobals.Instance.Variables.GetFsmBool("GUIdisassemble").Value = true;
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Detach();
+                    PlayMakerGlobals.Instance.Variables.GetFsmBool("GUIdisassemble").Value = false;
+                }
+            }
+        }
+
+        public void Attach()
+        {
+            ModConsole.Log("La");
+            transform.parent = pivot;
+            transform.localPosition = Vector3.zero;
+            transform.localEulerAngles = Vector3.zero;
+            transform.gameObject.tag = "Untagged";
+            Destroy(GetComponent<Rigidbody>());
+            MasterAudio.PlaySound3DAndForget("CarBuilding", transform, false, 1f, null, 0f, "assemble");
+            StartCoroutine(stay());
+            pivot.GetComponent<Collider>().enabled = false;
+            on = true;
+        }
+
+        void Detach()
+        {
+            StopCoroutine(stay());
+            transform.parent = null;
+            transform.gameObject.tag = "PART";
+            Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+            rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            MasterAudio.PlaySound3DAndForget("CarBuilding", transform, false, 1f, null, 0f, "disassemble");
+            pivot.GetComponent<Collider>().enabled = true;
+            on = false;
+        }
+
+        IEnumerator stay()
+        {
+            yield return new WaitForSeconds(0.1f);
+            transform.parent = pivot;
+            transform.localPosition = Vector3.zero;
+            transform.localEulerAngles = Vector3.zero;
+            transform.gameObject.tag = "Untagged";
+        }
+    }
+}
---
+++ AttachablePartApi/AttachablePartApi/AttachablePartApi.cs
@@ -0,0 +1,45 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace AttachablePartApi
+{
+    public class AttachablePartApi : Mod
+    {
+        public override string ID => "AttachablePartApi"; //Your mod ID (unique)
+        public override string Name => "AttachablePartApi"; //You mod name
+        public override string Author => "glen"; //Your Username
+        public override string Version => "1.0"; //Version
+        public override string Description => ""; //Short description of your mod
+
+        public override void ModSetup()
+        {
+            SetupFunction(Setup.PostLoad, Mod_PostLoad);
+            SetupFunction(Setup.OnSave, Mod_OnSave);
+        }
+
+        public override void ModSettings()
+        {
+            // All settings should be created here. 
+            // DO NOT put anything else here that settings or keybinds
+        }
+
+        private void Mod_PostLoad()
+        {
+                foreach (AttachComp attachable in GameObject.FindObjectsOfType<AttachComp>())
+                {
+                    if (SaveLoad.ReadValue<bool>(this, "attached"+attachable.gameObject.name))
+                    {
+                        attachable.Attach();
+                    }
+                }
+        }
+
+        private void Mod_OnSave()
+        {
+            foreach (AttachComp attachable in GameObject.FindObjectsOfType<AttachComp>())
+            {
+                SaveLoad.WriteValue(this, "attached"+attachable.gameObject.name, attachable.on);
+            }
+        }
+    }
+}
